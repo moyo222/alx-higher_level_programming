689450e7d6877673590c94714d73b32bc046c9d3
moyo222 <bankolemoyo5@gmail.com>
2023-06-11 14:43:31 -0700

    last correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-"""Print the number of arguments and list of arguments."""
+
+    """Print the number of arguments and list of arguments."""
+
 import sys
+
 count = len(sys.argv) - 1
+
 if count == 0:
-print("0 arguments.")
+
+    print("0 arguments.")
+
 elif count == 1:
-print("1 argument:")
+
+    print("1 argument:")
+
 else:
-print("{} arguments:".format(count))
+
+    print("{} arguments:".format(count))
+
 for i in range(count):
-print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
